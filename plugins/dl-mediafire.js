6e25b74f55353038f797aa7aedc295bc40336e56
King-pe <hackerattack61@gmail.com>
2025-09-27 07:10:55 -0700

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,20 +1,24 @@
-
-import fetch from 'node-fetch'
-import { mediafiredl } from '@bochilteam/scraper'
+const fetch = require('node-fetch');
+const { mediafiredl } = require('@bochilteam/scraper');
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
-	var limit
-     if((isOwner || isPrems)) limit = 1200
-     else limit = 100
-   if (!args[0]) throw `✳️ Enter the mediafire link next to the command`
-    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`
-    m.react(rwait)
-    let full = /f$/i.test(command)
-    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer()
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
+    var limit;
+    if ((isOwner || isPrems)) limit = 1200;
+    else limit = 100;
+
+    if (!args[0]) throw `✳️ Enter the mediafire link next to the command`;
+    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`;
+
+    m.react(rwait);
+    let full = /f$/i.test(command);
+    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0];
+
+    let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer();
+    let res = await mediafiredl(args[0]);
+    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res;
+
+    let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize;
+
     let caption = `
    ≡ *MEDIAFIRE*
 
@@ -23,16 +27,18 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) =
 ▢ *Extension:* ${ext}
 ▢ *Uploaded:* ${aploud}
 ${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade to premium to be able to download files more than *900 MB*` : ''} 
-`.trim()
-    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-    
-    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-}
-handler.help = ['mediafire <url>']
-handler.tags = ['downloader', 'premium']
-handler.command = ['mediafire', 'mfire'] 
-handler.credit = true
-handler.premium = false
+`.trim();
 
-export default handler
+    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m);
+
+    if (!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true });
+    m.react(done);
+};
+
+handler.help = ['mediafire <url>'];
+handler.tags = ['downloader', 'premium'];
+handler.command = ['mediafire', 'mfire'];
+handler.credit = true;
+handler.premium = false;
+
+module.exports = handler;
